806e69e9e173e2f4d34979ce21daf06af9af170d
Daniel Thornburgh <mysterymath@gmail.com>
2023-11-29 10:06:55 -0800

    Use lld

--- clang/lib/Driver/ToolChains/Microcosm.cpp
+++ clang/lib/Driver/ToolChains/Microcosm.cpp
@@ -1,11 +1,16 @@
 #include "Microcosm.h"
+#include "ToolChains/Arch/ARM.h"
+#include "ToolChains/CommonArgs.h"
 
-#include "llvm/Option/ArgList.h"
+#include "clang/Driver/Compilation.h"
 #include "clang/Driver/Driver.h"
+#include "llvm/Option/ArgList.h"
+#include "llvm/Support/Path.h"
 
 using namespace llvm::opt;
 using namespace clang;
 using namespace clang::driver;
+using namespace clang::driver::tools;
 using namespace clang::driver::toolchains;
 
 Microcosm::Microcosm(const Driver &D, const llvm::Triple &Triple,
@@ -14,4 +19,48 @@ Microcosm::Microcosm(const Driver &D, const llvm::Triple &Triple,
   getProgramPaths().push_back(getDriver().getInstalledDir());
   if (getDriver().getInstalledDir() != getDriver().Dir)
     getProgramPaths().push_back(getDriver().Dir);
+}
+
+Tool *Microcosm::buildLinker() const {
+  return new tools::microcosm::Linker(*this);
+}
+
+void microcosm::Linker::ConstructJob(Compilation &C, const JobAction &JA,
+                                     const InputInfo &Output,
+                                     const InputInfoList &Inputs,
+                                     const ArgList &Args,
+                                     const char *LinkingOutput) const {
+  ArgStringList CmdArgs;
+
+  auto &TC = static_cast<const toolchains::Microcosm &>(getToolChain());
+  const llvm::Triple &Triple = getToolChain().getEffectiveTriple();
+
+  AddLinkerInputs(TC, Inputs, Args, CmdArgs, JA);
+
+  if (Triple.isARM() || Triple.isThumb()) {
+    bool IsBigEndian = arm::isARMBigEndian(Triple, Args);
+    if (IsBigEndian)
+      arm::appendBE8LinkFlag(Args, CmdArgs, Triple);
+    CmdArgs.push_back(IsBigEndian ? "-EB" : "-EL");
+  }
+
+  Args.addAllArgs(CmdArgs, {options::OPT_L, options::OPT_T_Group,
+                            options::OPT_s, options::OPT_t, options::OPT_r});
+
+  TC.AddFilePathLibArgs(Args, CmdArgs);
+
+  for (const auto &LibPath : TC.getLibraryPaths())
+    CmdArgs.push_back(Args.MakeArgString(llvm::Twine("-L", LibPath)));
+
+  const std::string FileName = TC.getCompilerRT(Args, "builtins");
+  llvm::SmallString<128> PathBuf{FileName};
+  llvm::sys::path::remove_filename(PathBuf);
+  CmdArgs.push_back(Args.MakeArgString("-L" + PathBuf));
+
+  CmdArgs.push_back("-o");
+  CmdArgs.push_back(Output.getFilename());
+
+  C.addCommand(std::make_unique<Command>(
+      JA, *this, ResponseFileSupport::AtFileCurCP(),
+      Args.MakeArgString(TC.GetLinkerPath()), CmdArgs, Inputs, Output));
 }
--- clang/lib/Driver/ToolChains/Microcosm.h
+++ clang/lib/Driver/ToolChains/Microcosm.h
@@ -9,6 +9,7 @@
 #ifndef LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_MICROCOSM_H
 #define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_MICROCOSM_H
 
+#include "clang/Driver/Tool.h"
 #include "clang/Driver/ToolChain.h"
 
 namespace clang {
@@ -19,15 +20,39 @@ namespace toolchains {
 class LLVM_LIBRARY_VISIBILITY Microcosm : public ToolChain {
 public:
   Microcosm(const Driver &D, const llvm::Triple &Triple,
-           const llvm::opt::ArgList &Args);
+            const llvm::opt::ArgList &Args);
   ~Microcosm() override = default;
 
+protected:
+  Tool *buildLinker() const override;
+
+  const char *getDefaultLinker() const override { return "ld.lld"; }
+
   bool isPICDefault() const override { return true; }
-  bool isPIEDefault(const llvm::opt::ArgList &Args) const override { return true; }
+  bool isPIEDefault(const llvm::opt::ArgList &Args) const override {
+    return true;
+  }
   bool isPICDefaultForced() const override { return true; }
 };
 
 } // namespace toolchains
+
+namespace tools {
+namespace microcosm {
+
+class LLVM_LIBRARY_VISIBILITY Linker final : public Tool {
+public:
+  Linker(const ToolChain &TC) : Tool("microcosm::Linker", "ld.lld", TC) {}
+  bool isLinkJob() const override { return true; }
+  bool hasIntegratedCPP() const override { return false; }
+  void ConstructJob(Compilation &C, const JobAction &JA,
+                    const InputInfo &Output, const InputInfoList &Inputs,
+                    const llvm::opt::ArgList &TCArgs,
+                    const char *LinkingOutput) const override;
+};
+
+} // namespace microcosm
+} // namespace tools
 
 } // namespace driver
 } // namespace clang
